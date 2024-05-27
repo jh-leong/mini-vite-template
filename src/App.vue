0b78a31ebab0c7cdbbcc19826cecea90a9685284
tycho <jh.leong@outlook.com>
2024-05-27 10:05:23 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Foo from '@/components/Foo.vue';
+</script>
 
 <template>
   <div class="h-[100vh] flex items-center justify-center font-bold text-[52px]">
     Hello word!
+
+    <Foo></Foo>
   </div>
 </template>
 
