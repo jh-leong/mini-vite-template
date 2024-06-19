0b8a6fabae611cb44bc81d10acaafc30356c2851
tycho <jh.leong@outlook.com>
2024-06-19 16:09:56 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import Foo from '@/components/Foo.vue';
+import Foo from './Comp.vue';
 </script>
 
 <template>
   <div class="h-[100vh] flex items-center justify-center font-bold text-[52px]">
     Hello word!
-
     <Foo></Foo>
   </div>
 </template>
---
+++ src/Comp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">tycho</div>
+</template>
+
+<script setup lang="ts">
+// interface Props {
+//  foo: boolean;
+// }
+
+// const props = withDefaults(defineProps<Props>(), {
+//   foo: false,
+// });
+
+// const emit = defineEmits<{
+//   (e: 'foo'): void;
+// }>();
+</script>
+
+<style lang="scss" scoped></style>
