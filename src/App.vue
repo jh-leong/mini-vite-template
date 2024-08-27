518c36b2a6a8f1b06cfa026832cf8727bcc57627
tycho <jh.leong@outlook.com>
2024-08-27 14:16:04 +0800

    chore: udpate

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import Foo from './Comp.vue';
+import { ref } from 'vue';
+import Comp from 'Comp.vue';
+
+const msg = ref('Hello World!');
 </script>
 
 <template>
-  <div class="h-[100vh] flex items-center justify-center font-bold text-[52px]">
-    Hello word!
-    <Foo></Foo>
-  </div>
+  <h1 v-foo>{{ msg }}</h1>
+  <input v-model="msg" />
+  <Comp></Comp>
 </template>
-
-<style scoped></style>
